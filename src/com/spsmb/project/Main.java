6be92ad310c2f0d45027fc05c01778088c209aa2
eva.masakova <eva.masakova@post.cz>
2022-09-16 11:51:27 +0200

    Initial commit

---
+++ src/com/spsmb/project/Main.java
@@ -0,0 +1,13 @@
+package com.spsmb.project;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.println("Zadejte větu.");
+
+    }
+
+}
